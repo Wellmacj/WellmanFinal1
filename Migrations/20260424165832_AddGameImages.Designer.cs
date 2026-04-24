1e931eac569580b3918001c45b5bdc4ad2d5cf84
Wellmacj <wellmacj@mail.uc.edu>
2026-04-24 13:01:34 -0400

    Added migration for image path so all images have specialized paths for game photo

--- Migrations/20260424165832_AddGameImages.Designer.cs
+++ Migrations/20260424165832_AddGameImages.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WellmanFinal1.Data;
 
@@ -10,9 +11,11 @@ using WellmanFinal1.Data;
 namespace WellmanFinal1.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260424165832_AddGameImages")]
+    partial class AddGameImages
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Models/Game.cs
+++ Models/Game.cs
@@ -14,5 +14,7 @@ namespace WellmanFinal1.Models
         public int Rating { get; set; }
 
         public int HoursPlayed { get; set; }
+
+        public string ImagePath { get; set; }
     }
 }
