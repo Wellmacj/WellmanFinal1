594b730bca3c3b1952704af3989270a4c4d2f68b
Wellmacj <wellmacj@mail.uc.edu>
2026-04-24 12:32:53 -0400

    Updated Game Controller to update with data

--- Controllers/GamesController.cs
+++ Controllers/GamesController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WellmanFinal1.Data;
 
 namespace WellmanFinal1.Controllers
 {
     public class GamesController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public GamesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var games = _context.Games.ToList();
+            return View(games);
         }
     }
 }
