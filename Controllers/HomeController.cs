6a7b465ddfc6978f970a3607250c37ffa38da7da
Wellmacj <wellmacj@mail.uc.edu>
2026-04-24 13:24:46 -0400

    removed privacy

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,11 +18,6 @@ namespace WellmanFinal1.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         public IActionResult AboutMe()
         {
             return View();
