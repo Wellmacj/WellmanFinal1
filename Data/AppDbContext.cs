b80c28ed0e2cd4579a90a84be70b10d7b14627e5
Wellmacj <wellmacj@mail.uc.edu>
2026-04-24 12:30:30 -0400

    Added Entity Framework core, tools, and sql. Added Database with Game Model and Migrations.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using WellmanFinal1.Models;
+
+namespace WellmanFinal1.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Game> Games { get; set; }
+    }
+}
---
+++ Models/Game.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WellmanFinal1.Models
+{
+    public class Game
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Genre { get; set; }
+
+        public int Rating { get; set; }
+
+        public int HoursPlayed { get; set; }
+    }
+}
