92768fca22a4a88d5b32def3f96fa9baaed7224b
Wellmacj <wellmacj@mail.uc.edu>
2026-04-24 12:24:37 -0400

    Added about me and navigation to games and about me pages

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace WellmanFinal1.Controllers
             return View();
         }
 
+        public IActionResult AboutMe()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
